8d52cbaa1e9b3a84dbc318fc56931c4ecd61a18a
Adam Pohorecki <adam.pohorecki@llp.pl>
2012-02-09 19:37:25 +0100

    The app is started in tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,9 @@ require 'capybara'
 require 'capybara/dsl'
 require 'cucumber/websteps'
 
+require_relative '../../app'
+
 Capybara.default_selector = :css
+Capybara.app = App
 
 World(Capybara::DSL)
